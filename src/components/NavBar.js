8cd7636a0dd3f7eadd12adda21a5f0269c01d28d
Kaleshe <kalesheav@gmail.com>
2024-03-11 10:55:30 +0000

    refactor: use a hook for auth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,12 @@
 "use client";
 
 import React from "react";
-
 import Link from "next/link";
-import isAuth from "../helpers/isAuth";
 
-import Secure from "../utils/SecureLs";
-
+import useAuth from "@/hooks/useAuth";
 
 const NavBar = () => {
+  const { user, handleLogout, startAuth } = useAuth();
 
   return (
     <nav className="bg-white dark:bg-slate-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
@@ -23,13 +21,16 @@ const NavBar = () => {
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-800 dark:focus:ring-blue-800 dark:text-gray-300"
+              onClick={startAuth}
             >
               Login
             </button>
           )}
           {user && (
             <div className="flex items-center md:order-2">
-              <button type="button">Logout</button>
+              <button type="button" onClick={handleLogout}>
+                Logout
+              </button>
             </div>
           )}
         </div>
