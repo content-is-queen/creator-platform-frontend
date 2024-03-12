e75a45ba88f6af9f7f726798a967b166ad7fdd6d
Kaleshe <kalesheav@gmail.com>
2024-03-12 11:46:34 +0000

    feat: opportunities search

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,12 @@
 import clsx from "clsx";
 
-const Container = ({ className, children }) => (
-  <div className={clsx("max-w-6xl mx-auto px-8", className)}>{children}</div>
+const SIZES = {
+  lg: "max-w-2xl",
+  default: "max-w-6xl",
+};
+
+const Container = ({ size = "default", className, children }) => (
+  <div className={clsx(SIZES[size], "mx-auto px-8", className)}>{children}</div>
 );
 
 export default Container;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,22 @@
-const Input = ({ children, name, ...otherProps }) => (
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const Input = ({
+  children,
+  icon,
+  name,
+  className,
+  label = true,
+  ...otherProps
+}) => (
   <div className="relative z-0 w-full group">
     <input
-      className="block py-3.5 px-0 w-full text-sm text-queen-black bg-transparent border-0 border-b-2 border-queen-black appearance-none focus:outline-none focus:ring-0 focus:border-queen-blue peer"
+      className={clsx(
+        twMerge(
+          "placeholder:uppercase placeholder:text-queen-black block py-3.5 px-0 w-full text-sm text-queen-black bg-transparent border-0 border-b-2 border-queen-black appearance-none focus:outline-none focus:ring-0 focus:border-queen-blue peer",
+          className
+        )
+      )}
       name={name}
       placeholder=" "
       id={name}
@@ -9,10 +24,14 @@ const Input = ({ children, name, ...otherProps }) => (
     />
     <label
       htmlFor={name}
-      className="peer-focus:font-medium absolute duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-queen-blue peer-placeholder-shown:scale-100 peer-placeholder-shown:text-queen-black/60 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 uppercase"
+      className={clsx(
+        "peer-focus:font-medium absolute duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-queen-blue peer-placeholder-shown:scale-100 peer-placeholder-shown:text-queen-black/60 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 uppercase",
+        !label && "sr-only"
+      )}
     >
       {children}
     </label>
+    {icon && icon}
   </div>
 );
 
