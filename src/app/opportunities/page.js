3536f9ee910b4521e3523a9a4f1565c71eff8bce
bizip <bizip04@gmail.com>
2024-03-14 09:01:04 +0200

    Fix conflict from package.json

--- src/app/opportunities/page.js
+++ src/app/opportunities/page.js
@@ -1,10 +1,20 @@
+import Heading from "@/components/Heading";
 import MainNav from "@/components/MainNav";
+import Container from "@/components/Container";
+import Search from "@/components/Search";
+
+const TAGS = ["pitch", "broadcasting", "business", "publishing"];
 
 const Opportunities = () => (
-  <section>
+  <div>
     <MainNav />
-    <h1>This is the opportunities page</h1>
-  </section>
+    <Container size="lg">
+      <div className="text-center mt-20 mb-16">
+        <Heading>Opportunities</Heading>
+      </div>
+      <Search tags={TAGS} />
+    </Container>
+  </div>
 );
 
 export default Opportunities;
