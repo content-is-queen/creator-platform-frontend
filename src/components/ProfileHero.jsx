8c543baf017ce19edb07895a1c3ad9c57048cb50
Kaleshe <kalesheav@gmail.com>
2024-05-09 10:11:54 +0100

    fix: import error

--- src/components/ProfileHero.jsx
+++ src/components/ProfileHero.jsx
@@ -4,7 +4,7 @@ import ProfileIcon from "@/components/ProfileIcon";
 import Dots from "@/components/Patterns/Dots";
 import Container from "@/components/Container";
 import Tag from "@/components/Tag";
-import LoadingText from "@/components/LoadingText";
+import LoadingPlaceholder from "@/components/LoadingPlaceholder";
 
 const ProfileHero = ({ user, children }) => {
   return (
@@ -15,7 +15,11 @@ const ProfileHero = ({ user, children }) => {
 
         <div className="max-w-96">
           <h1 className="font-heading uppercase text-2xl">
-            {!user ? <LoadingText /> : `${user?.first_name} ${user?.last_name}`}
+            {!user ? (
+              <LoadingPlaceholder />
+            ) : (
+              `${user?.first_name} ${user?.last_name}`
+            )}
           </h1>
           {user?.tags && (
             <div className="flex gap-2 my-2">
