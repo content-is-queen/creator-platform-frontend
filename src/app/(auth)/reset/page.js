83f6078c424f814f475490325ac2cd701fb48747
bizip <bizip04@gmail.com>
2024-05-10 11:46:48 +0200

    Add reset password form

---
+++ src/app/(auth)/reset/page.js
@@ -0,0 +1,13 @@
+"use client"
+import Heading from "@/components/Heading";
+import ResetPasswordForm from "@/components/ResetPasswordForm";
+import React from "react";
+
+const reset = () => {
+  return <>
+  <Heading>Reset your password</Heading>
+  <ResetPasswordForm />
+  </>;
+};
+
+export default reset;
--- src/components/ResetPasswordForm.jsx
+++ src/components/ResetPasswordForm.jsx
@@ -10,6 +10,9 @@ import AuthInput from "@/components/AuthInput";
 
 import useAuth from "@/hooks/useAuth";
 import { useUser } from "@/context/UserContext";
+import API from "@/api/api";
+import { useState } from "react";
+import useToken from "@/hooks/useToken";
 
 const FIELDS = [
   {
@@ -23,43 +26,55 @@ const FIELDS = [
         message: "Invalid email address",
       },
     },
-  },
-  {
-    name: "password",
-    type: "password",
-    children: "Password",
-    rules: {
-      required: "Password is required",
-      minLength: {
-        value: 6,
-        message: "Password must be at least 6 characters",
-      },
-    },
-  },
+  }
 ];
 
-const LoginForm = () => {
+const ResetPasswordForm = () => {
+  const { token } = useToken();
   const {
     handleSubmit,
     control,
     formState: { errors },
   } = useForm();
 
-  const router = useRouter();
-  const { signin } = useAuth();
-  const { user } = useUser();
+  const [errorss, setError] = useState({});
 
   const onSubmit = async (data) => {
+    console.log(data);
+    setError({});
     try {
-      await signin(data.email, data.password);
+      const response = await API(`/auth/reset`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.status > 200) {
+        setError({
+          message: "Something went wrong. User sign up failed.",
+        });
+        return;
+      }
+      return response;
     } catch (error) {
-      console.error(error);
+      setError({
+        message: "Something went wrong. User sign up failed.",
+      });
+      console.error("Sign up error:", error);
     }
+
+
   };
 
   return (
+    <>
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-6">
+      <Text size="sm" className="!mt-2">
+      Enter your email and we’ll send you a link to reset your password
+        </Text>
         {FIELDS.map(({ children, name, ...otherProps }) => (
           <AuthInput
             key={name}
@@ -71,21 +86,19 @@ const LoginForm = () => {
             {children}
           </AuthInput>
         ))}
-        <Text size="sm" className="!mt-2">
-          <Link href="/reset">Forgot password?</Link>
-        </Text>
+
       </div>
       <Button as="button" type="submit" className="mt-8">
-        Sign in
+      send password recovery link
       </Button>
-      <Text size="sm" className="mt-4">
-        Don&apos;t have an account?{" "}
-        <Link href="/signup" className="font-medium text-queen-blue">
-          Sign up
-        </Link>
-      </Text>
     </form>
+    {errorss?.message && (
+      <div className="border border-red-700 bg-red-100 text-red-700 text-sm mt-4 py-2 px-4">
+        <p>{errorss.message}</p>
+      </div>
+    )}
+    </>
   );
 };
 
-export default LoginForm;
+export default ResetPasswordForm;
