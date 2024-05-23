72f37b7568078e0c50ad0cd994d489b7f082447f
bizip <bizip04@gmail.com>
2024-05-23 11:28:38 +0200

    Add an eye icon to signup page

--- src/components/SignUpForm/SignUpFormStep.jsx
+++ src/components/SignUpForm/SignUpFormStep.jsx
@@ -4,7 +4,7 @@ import AuthRadioController from "@/components/AuthRadioController";
 const SignUpFormStep = ({ fields, errors, control }) => {
   return (
     <div className="space-y-6">
-      {fields.map(({ children, options, name, ...otherProps }) => {
+      {fields.map(({ children, options, name, type, ...otherProps }) => {
         let Component;
 
         switch (name) {
