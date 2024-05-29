adc8813090fa0141e321aacc130d43684a2804dd
Kaleshe <kalesheav@gmail.com>
2024-05-29 19:49:05 +0100

    feat: conversations styling

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-import API from "@/api/api";
 import { useUser } from "@/context/UserContext";
 
 import {
@@ -8,8 +7,10 @@ import {
   orderBy,
   onSnapshot,
   query,
+  doc,
   limit,
   addDoc,
+  updateDoc,
   serverTimestamp,
 } from "firebase/firestore";
 import { db } from "@/firebase.config";
@@ -26,13 +27,13 @@ import ProfileIcon from "./ProfileIcon";
 
 const Message = ({ children, currentUser }) => {
   return (
-    <div className={clsx("flex items-center", !currentUser && "justify-end")}>
-      {!currentUser && <ProfileIcon className="h-10 w-10 mr-2 " />}
+    <div className={clsx("flex items-center", currentUser && "justify-end")}>
+      {!currentUser && <ProfileIcon className="mr-2" />}
       <div
         className={clsx(
           "py-3 px-4 text-queen-black",
-          !currentUser
-            ? "bg-queen-gray rounded-bl-3xl rounded-tl-3xl rounded-tr-xl"
+          currentUser
+            ? "bg-queen-gray/80 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl"
             : "bg-queen-white rounded-br-3xl rounded-tr-3xl rounded-tl-xl"
         )}
       >
@@ -42,18 +43,20 @@ const Message = ({ children, currentUser }) => {
   );
 };
 
-const Header = () => (
-  <div className="sticky top-0 bg-whiteflex items-center space-x-4 rtl:space-x-reverse hover:rounded-lg border-solid">
+const Header = ({ room }) => (
+  <div className="sticky top-0 flex items-center space-x-4 rtl:space-x-reverse border-solid px-8 py-4 shadow-sm">
     <div>
       <div className="flex-shrink-0">
-        <ProfileIcon className="h-12 w-12 flex-shrink-0 lg:w-16 lg:h-16 " />
+        <ProfileIcon className="h-12 w-12 flex-shrink-0" />
       </div>
     </div>
-    <div className="flex-1 min-w-0">
-      <p className="font-medium text-gray-900 truncate">
-        {/* {getchatIds.fullName} */}
-      </p>
-      <p className="text-sm text-gray-500 truncate">{/* Opportunity name */}</p>
+    <div>
+      <span className="text-gray-900 truncate font-subheading font-bold block leading-4">
+        {room.fullName}
+      </span>
+      <span className="text-sm text-gray-500 truncate block">
+        Opportunity name
+      </span>
     </div>
   </div>
 );
@@ -62,6 +65,7 @@ const Body = ({ room }) => {
   const { user } = useUser();
 
   const [messages, setMessages] = useState([]);
+  const messageBody = useRef();
 
   useEffect(() => {
     const q = query(
@@ -81,8 +85,14 @@ const Body = ({ room }) => {
     return () => unsubscribe;
   }, []);
 
+  useEffect(() => {
+    // Always scroll to the latest message
+    if (messageBody)
+      messageBody.current.scrollTop = messageBody.current.scrollHeight;
+  }, [messages]);
+
   return (
-    <div>
+    <div className="h-[80%] overflow-y-auto p-8" ref={messageBody}>
       <div className="w-full flex flex-col justify-between">
         <div className="flex flex-col space-y-8">
           {messages.length > 0 &&
@@ -112,6 +122,16 @@ const Footer = ({ room }) => {
       uid: user.uid,
       createdAt: serverTimestamp(),
     });
+
+    const roomRef = doc(db, "rooms", room.id);
+
+    await updateDoc(roomRef, {
+      ...room,
+      lastMessage: message,
+      senderId: user.uid,
+      timeSent: serverTimestamp(),
+    });
+
     setMessage("");
   };
 
@@ -120,36 +140,38 @@ const Footer = ({ room }) => {
   };
 
   return (
-    <div className="bg-gradient-to-t from-white from-40% to-transparent bottom-0 w-full flex relative mt-auto">
-      <input
-        type="text"
-        placeholder="Enter your message"
-        className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-8 rounded-full py-2 border border-queen-black/20"
-        value={message}
-        onChange={handleChange}
-      />
-      <div className="absolute right-0 pr-4 items-center inset-y-0 hidden sm:flex">
-        <button
-          type="button"
-          className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-queen-black hover:opacity-80 focus:outline-none"
-        >
-          <FontAwesomeIcon className="h-6 w-6" icon={faImage} />
-        </button>
-        <button
-          type="button"
-          className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-queen-black hover:opacity-80 focus:outline-none"
-        >
-          <FontAwesomeIcon className="h-6 w-6" icon={faPaperclip} />
-        </button>
-        <Button
-          as="button"
-          type="button"
-          onClick={sendMessage}
-          className="inline-flex items-center gap-2 h-8 w-8 px-0 justify-center rounded-full"
-        >
-          <span className="sr-only">Send</span>
-          <FontAwesomeIcon className="shrink-0 h-4 w-4" icon={faPaperPlane} />
-        </Button>
+    <div className="relative ">
+      <div className="w-full p-8">
+        <input
+          type="text"
+          placeholder="Enter your message"
+          className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-8 rounded-full py-2 border border-queen-black/20"
+          value={message}
+          onChange={handleChange}
+        />
+        <div className="absolute right-6 pr-4 items-center inset-y-0 hidden sm:flex">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-queen-black hover:opacity-80 focus:outline-none"
+          >
+            <FontAwesomeIcon className="h-6 w-6" icon={faImage} />
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-queen-black hover:opacity-80 focus:outline-none"
+          >
+            <FontAwesomeIcon className="h-6 w-6" icon={faPaperclip} />
+          </button>
+          <Button
+            as="button"
+            type="button"
+            onClick={sendMessage}
+            className="inline-flex items-center gap-2 h-8 w-8 px-0 justify-center rounded-full"
+          >
+            <span className="sr-only">Send</span>
+            <FontAwesomeIcon className="shrink-0 h-4 w-4" icon={faPaperPlane} />
+          </Button>
+        </div>
       </div>
     </div>
   );
@@ -158,14 +180,14 @@ const Footer = ({ room }) => {
 const SingleChat = ({ room }) => {
   return (
     <div
-      className="relative col-span-4 bg-white rounded-3xl shadow-md"
+      className="relative col-span-8 bg-white rounded-3xl shadow-md"
       style={{
         height: "calc(100vh - var(--nav-height) - 54px)",
         maxHeight: "calc(100vh - var(--nav-height) - 54px)",
       }}
     >
-      <div className="flex items-stretch flex-col h-[inherit]">
-        <Header />
+      <div className="flex items-stretch flex-col h-[inherit] rounded-t-xl ">
+        <Header room={room} />
         <Body room={room} />
         <Footer room={room} />
       </div>
