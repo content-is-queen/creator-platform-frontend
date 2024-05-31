0a9a240096ac1f4053411d69249a31ad72ae7865
Kaleshe <kalesheav@gmail.com>
2024-05-31 10:03:09 +0100

    refactor: CheckoutForm

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,18 +1,16 @@
 "use client";
+
 import { useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import API from "@/api/api";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_PK_TEST
-);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_PK_TEST);
 
-const CheckoutForm = ({ userId }) => {
+const CheckoutForm = () => {
   const [loading, setLoading] = useState(false);
 
   const handleCheckout = async () => {
     setLoading(true);
-    console.log("handleCheckout function called");
 
     try {
       const response = await API.post("/payments/create-checkout-session", {});
@@ -22,7 +20,6 @@ const CheckoutForm = ({ userId }) => {
       }
 
       const sessionId = response.data.id; // Extract session ID from response data
-      console.log("Checkout session ID:", sessionId);
 
       const stripe = await stripePromise;
 
@@ -33,20 +30,18 @@ const CheckoutForm = ({ userId }) => {
 
       if (error) {
         throw new Error("An error occurred during checkout");
+      }
+
+      // Call API to update user subscription status upon successful checkout
+      const updateResponse = await API.post(
+        `/auth/userSubscription/sCxCtR8YPfWyrZGVkwyzcTNcF5N2`,
+        { subscribed: true }
+      );
+
+      if (updateResponse.status === 200) {
+        console.log("User subscription status updated successfully");
       } else {
-        console.log("Redirecting to checkout...");
-
-        // Call API to update user subscription status upon successful checkout
-        const updateResponse = await API.post(
-          `/auth/userSubscription/sCxCtR8YPfWyrZGVkwyzcTNcF5N2`,
-          { subscribed: true }
-        );
-
-        if (updateResponse.status === 200) {
-          console.log("User subscription status updated successfully");
-        } else {
-          throw new Error("Failed to update user subscription status");
-        }
+        throw new Error("Failed to update user subscription status");
       }
     } catch (error) {
       console.error("Checkout error:", error);
