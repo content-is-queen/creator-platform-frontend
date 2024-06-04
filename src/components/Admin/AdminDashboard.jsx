9d61f8ba8274cd76be413dff7c022f6ae7cda65a
Kaleshe <kalesheav@gmail.com>
2024-06-04 22:45:12 +0100

    wip: trying to get info to work

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
+
+import API from "@/api/api";
+import useToken from "@/hooks/useToken";
+
 import Heading from "../Heading";
 import InfoCard from "../InfoCard";
 
-const getAppData = async () => {};
+const AdminDashboard = () => {
+  const [data, setData] = useState([]);
+  const { token } = useToken();
 
-const AdminDashboard = async () => {
-  const appData = await getAppData();
+  useEffect(() => {
+    if (token) {
+      (async () => {
+        try {
+          const { data } = await API("/admin/info", {
+            "Content-Type": "application/json",
+
+            Authorization: `Bearer ${token}`,
+          });
+          console.log(data);
+          setData(data);
+        } catch (error) {
+          console.log("Error: Getting app data:", error);
+        }
+      })();
+    }
+    console.log(token);
+  }, [token]);
+
   return (
     <section className="pl-[18%] pr-[18%] pt-10">
       <Heading>Overview</Heading>
 
       <div className="grid grid-cols-4 gap-x-4 gap-y-8 pt-8">
-        {infoData.map((info, index) => (
+        {data.map((info, index) => (
           <InfoCard key={index} title={info.title} value={info.value} />
         ))}
       </div>
