64c0df7bd1edac90989c45d4f52dcdd54af3ecef
Kaleshe <kalesheav@gmail.com>
2024-06-06 20:13:06 +0100

    fix: build error

--- src/app/(dashboard)/thankyou/page.js
+++ src/app/(dashboard)/thankyou/page.js
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 import API from "@/api/api";
 import useToken from "@/hooks/useToken";
