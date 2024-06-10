9e2f96e73cc7de65f283437c712e07315489f870
Mundurujoana <mundujoana@com>
2024-06-10 15:15:31 +0200

    Fix form submission handling: rename variables, handle errors, and update data handling

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -1,16 +1,7 @@
-import { Fragment, useState } from 'react';
-import { Menu } from '@headlessui/react';
-
 const Item = ({ children }) => (
-  <Menu.Item>
-    {({ active }) => (
-      <li
-        className={`font-medium hover:bg-gray-50 hover:text-queen-blue ${active ? 'bg-gray-50' : ''}`}
-      >
-        {children}
-      </li>
-    )}
-  </Menu.Item>
+  <li className="font-medium hover:bg-gray-50 hover:text-queen-blue">
+    {children}
+  </li>
 );
 
 const Heading = ({ children }) => (
@@ -20,19 +11,16 @@ const Heading = ({ children }) => (
 );
 
 const SubMenu = ({ heading, children }) => (
-  <Menu>
-    {({ open }) => (
-      <Fragment>
-        {heading && heading}
-        <Menu.Items className="z-50 text-sm list-none text-queen-black bg-white divide-y divide-gray-100 rounded-lg shadow-md absolute top-14 right-0">
-          {children}
-        </Menu.Items>
-      </Fragment>
-    )}
-  </Menu>
+  <div className="z-50 text-sm list-none text-queen-black bg-white divide-y divide-gray-100 rounded-lg shadow-md absolute top-14 right-0">
+    {heading && heading}
+    <ul className="py-2" aria-labelledby="user-menu-button">
+      {children}
+    </ul>
+  </div>
 );
 
 export default SubMenu;
 
 SubMenu.Heading = Heading;
+
 SubMenu.Item = Item;
