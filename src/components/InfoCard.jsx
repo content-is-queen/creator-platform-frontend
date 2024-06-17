d7adde6d310f0067bceea6751a0b14a4e170b105
Kaleshe <kalesheav@gmail.com>
2024-06-17 14:31:53 +0100

    fix: add conditional value to info card

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,12 +1,12 @@
 import Card from "./Card";
 import Subheading from "./Subheading";
 
-const InfoCard = ({ title, children }) => {
+const InfoCard = ({ title, value, children }) => {
   return (
     <Card className="flex flex-col justify-between">
       <dd className="text-queen-black/80 pb-2">{title}</dd>
       <Subheading as="dt" size="4xl">
-        {children}
+        {value || children}
       </Subheading>
     </Card>
   );
