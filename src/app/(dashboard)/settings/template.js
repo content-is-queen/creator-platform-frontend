7c451b3ae65e15b816ac1fbd61eb0ce8ce1f4e1d
Mundurujoana <mundujoana@com>
2024-06-24 08:50:49 +0200

    Add field for creators to update their interests

--- src/app/(dashboard)/settings/template.js
+++ src/app/(dashboard)/settings/template.js
@@ -2,27 +2,16 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
-import Container from "@/components/Container";
-import Text from "@/components/Text";
 import { useUser } from "@/context/UserContext";
-import API from "@/api/api";
-import useToken from "@/hooks/useToken";
-import Modal from "@/components/Modal";
-import Button from "@/components/Button";
-import { Error, Success } from "@/components/Form";
+import useAuth from "@/hooks/useAuth";
+
+import Container from "@/components/Container";
+import Subheading from "@/components/Subheading";
 
 const Template = ({ children }) => {
   const pathname = usePathname();
   const { user } = useUser();
-  const [fullName, setFullName] = useState("");
-  const [errors, setError] = useState({});
-  const { token } = useToken();
-  const [isOpen, setIsOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  console.log("User's input name:", fullName);
-
+  const { subscribed } = useAuth();
   const LINKS = [
     {
       href: "/settings",
@@ -36,7 +25,7 @@ const Template = ({ children }) => {
       href: "/settings/password",
       label: "Password",
     },
-    ...(user && user.role !== "admin" && user.role !== "super_admin"
+    ...(user && subscribed
       ? [
           {
             href: "/settings/subscription",
@@ -54,35 +43,15 @@ const Template = ({ children }) => {
       : []),
   ];
 
-  const handleDeleteAccount = async (e) => {
-    e.preventDefault();
-    if (fullName !== user.fullName) {
-      setError("Full name does not match. Please try again.");
-      return;
-    }
-
-    setLoading(true);
-
-    try {
-      await API.post("/delete-account", { token });
-      // Handle account deletion success (e.g., log out the user, redirect, etc.)
-    } catch (error) {
-      console.error("Failed to delete account:", error);
-      setError("An error occurred. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <Container size="4xl">
       <div className="py-12 md:py-20">
         <div className="flex gap-12">
           <div className="w-full max-w-40">
             <div className="border-b border-queen-black/20 pb-4 mb-4">
-              <Text size="lg" className="font-subheading font-bold mb-2">
+              <Subheading size="lg" className="mb-2">
                 Settings
-              </Text>
+              </Subheading>
               <ul>
                 {LINKS.map(({ href, label }) => (
                   <li key={href} className="py-1">
@@ -96,45 +65,13 @@ const Template = ({ children }) => {
                 ))}
               </ul>
             </div>
-            <button
-              type="button"
-              className="text-red-600"
-              onClick={() => setIsOpen(true)}
-            >
+            <button type="button" className="text-red-600">
               Delete account
             </button>
           </div>
           <div className="w-full">{children}</div>
         </div>
       </div>
-
-      <Modal
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        title="Confirm Account Deletion"
-        size="2xl"
-      >
-        <form onSubmit={handleDeleteAccount} className="mt-10">
-          <div className="space-y-6">
-            <p className="mb-4">
-              Please type your full name to confirm you want to delete your
-              account. This action cannot be undone.
-            </p>
-            <input
-              type="text"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-              placeholder="Full Name"
-              className="w-full p-2 border border-gray-300 rounded mb-4"
-            />
-            {errors?.message && <Error>{errors.message}</Error>}
-          </div>
-          <Button as="button" type="submit" className="mt-8">
-            {loading && <Button.Spinner />}
-            Confirm
-          </Button>
-        </form>
-      </Modal>
     </Container>
   );
 };
