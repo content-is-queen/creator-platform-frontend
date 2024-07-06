8c090de3d42b5fc73a5c6c609899fee71224607e
Kaleshe <kalesheav@gmail.com>
2024-07-06 13:50:56 +0100

    tweak: update CreatorDashboard dot color

--- src/components/Creator/CreatorDashboard.jsx
+++ src/components/Creator/CreatorDashboard.jsx
@@ -39,7 +39,7 @@ const CreatorDashboard = () => {
         style={{
           minHeight: "calc(100vh - var(--nav-height))",
           backgroundSize: "500px",
-          backgroundImage: "url('/images/CiQ_Pattern 4 orange.svg')",
+          backgroundImage: "url('/images/CiQ_Pattern 4 lilac.svg')",
         }}
         className="flex justify-center items-center py-12 text-center bg-queen-blue bg-repeat-x bg-[center_bottom_-2rem]"
       >
