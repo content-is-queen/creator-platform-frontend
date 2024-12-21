679212b92ef704da72af10ae5ec8e59d7e8cd884
Kaleshe <kaleshe@neuralalpha.com>
2024-12-21 17:16:35 +0000

    fix: stop refetch on window focus

--- src/components/Creator/CreatorApplications.jsx
+++ src/components/Creator/CreatorApplications.jsx
@@ -17,6 +17,7 @@ const CreatorApplications = () => {
       const { data } = await API.get(`applications/user/${user.uid}`);
       return data.message;
     },
+    refetchOnWindowFocus: false,
   });
   return (
     applications?.length > 0 && (
